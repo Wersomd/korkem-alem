c0a44419ebad33f23e64c67869141b64378f9b9a
Kazybek <ileskazybek@gmail.com>
2022-09-12 19:00:49 +0600

    Mobile version

--- public/js/team.js
+++ public/js/team.js
@@ -5,5 +5,21 @@ const swiper = new Swiper('.team__swiper', {
 
     scrollbar: {
       el: '.swiper-scrollbar',
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        spaceBetween: 20,
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      }
     }
   });
